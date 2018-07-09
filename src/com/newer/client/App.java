bc56fb471978a1417c616f2404197abd9c0124bc
jacktangcs <3240552533@qq.com>
2018-07-09 20:38:11 +0800

    实现了服务端类和客户端类

--- src/com/newer/client/App.java
+++ src/com/newer/client/App.java
@@ -1,14 +1,78 @@
 package com.newer.client;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 /**
- * 汤振
+ * 客户端
  * 
- * @author 
+ * @author 汤振
  *
  */
 public class App {
 
-	public static void main(String[] args) {
+	//定义客户端套接字
+	Socket socket;
+	
+	//定义端口
+	int port = 9000;
+	
+	//定义服务地址IP(默认本地地址)
+	String serverAddress = "";
+	
+	//定义文件输入流
+	FileInputStream fileIn;
+	
+	//定义输出流
+	OutputStream out;
+	
+	//定义客户端需要实现的任务
+	public void start() {
+		//获得要上传的文件
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("请输入文件绝对路径:");
+		String file = scanner.next();
+		scanner.close();
 		
+		 try {
+			 //通过服务地址和端口建立链接
+			socket =  new Socket(serverAddress, port);
+			out = socket.getOutputStream();
+			byte[] buf = new byte[1024*4];
+			int size;
+			fileIn = new FileInputStream(file);
+			while(-1!=(size = fileIn.read(buf))) {
+				out.write(buf, 0, size);
+				out.flush();
+			}
+			System.out.println("文件发送完毕");
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			try {
+				fileIn.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			try {
+				socket.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	public static void main(String[] args) {
+		App clientApp = new App();
+		clientApp.start();
 	}
 }
--- src/com/newer/fileserver/App.java
+++ src/com/newer/fileserver/App.java
@@ -1,5 +1,19 @@
 ﻿package com.newer.fileserver;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigInteger;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * 服务端
  * 
@@ -8,4 +22,89 @@
  */
 public class App {
 
+	//服务端套接字
+	ServerSocket serverSocket;
+	
+	//定义服务端端口
+	int port = 9000;
+	
+	//定义线程池
+	ExecutorService pool;
+	
+	//定义文件保存地址
+	String filePath = "E://files";
+	public void start() {
+		try {
+			serverSocket = new ServerSocket(port);
+			pool = Executors.newCachedThreadPool();
+
+			while(true) {
+				Socket socket = serverSocket.accept();
+				pool.execute(new Runnable() {
+					
+					@Override
+					public void run() {
+						ByteArrayOutputStream data = new ByteArrayOutputStream();
+						try(InputStream in = socket.getInputStream()) {
+							byte[] buf = new byte[1024*4];
+							int size;
+							while(-1!=(size = in.read(buf))) {
+								data.write(buf, 0, size);
+							}
+						} catch (Exception e) {
+							// TODO: handle exception
+						}
+						//接收到的数据
+						byte[] info = data.toByteArray();
+						
+						//利用SHA-256生产的16进制散列值作为文件名
+						String fileName = "";
+						
+						byte[] hash = null;
+						
+						//获得文件的散列值,定义文件名
+						try {
+							hash = MessageDigest.getInstance("SHA-256").digest(info);
+							fileName = new BigInteger(1,hash).toString(16);
+							
+						} catch (NoSuchAlgorithmException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+				
+						HashMap<byte[], String> map = new HashMap<byte[], String>();
+						if(!map.containsKey(hash)) {
+							map.put(hash, fileName);
+							try(FileOutputStream out = new FileOutputStream(new File(filePath, fileName))) {
+								out.write(info);
+								out.flush();
+								System.out.println("上传完成");
+							} catch (Exception e) {
+								System.out.println("上传失败");
+							}
+						}else {
+							System.out.println("文件已经存在，请不要重复上传");
+							return;
+						}
+						
+						//接收文件
+//						try(FileOutputStream out = new FileOutputStream(new File(filePath, fileName))) {
+//							out.write(info);
+//							out.flush();
+//							System.out.println("上传完成");
+//						} catch (Exception e) {
+//							System.out.println("上传失败");
+//						}
+					}
+				});
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public static void main(String[] args) {
+		App serverApp = new App();
+		serverApp.start();
+	}
 }
